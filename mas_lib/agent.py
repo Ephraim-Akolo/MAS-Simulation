99da8a9b472d67c6ea2f163f88b176e7b0f37a3b
jake-ephraim <ephraimakolo2017@gmail.com>
2022-09-29 14:26:39 +0100

    created bus  agent

--- mas_lib/agent.py
+++ mas_lib/agent.py
@@ -4,15 +4,16 @@ from time import sleep
 
 
 class AgentCB(ComBase):
-    _extras = None
+    _callback = None
     _state = None
     switch = {0: "broken", 1: "live"}
     neighbors = None
-    buses_volatage_rule = {"min": 0, "max": 33}
-    def __init__(self, name:str, host:str="", port:int=10001, state:int=1, extras:dict=None) -> None:
+    buses_volatage_rule = {"min": 10, "max": 33}
+    def __init__(self, name:str, host:str="", port:int=10001, state:int=1, non_blocking_callback=None) -> None:
         super().__init__(name, host, port)
         self.neighbors = MyParser().get_neighbors(name)
         self._state = self.switch[state]
+        self._callback = non_blocking_callback
     
     def broadcast(self, name, state):
         if self.neighbors:
@@ -24,9 +25,9 @@ class AgentCB(ComBase):
     
     def _affected_action(self, name, state):
         if len(name) == 2 and name[0] == "B":
-            voltage = int(state)
+            voltage = float(state)
             if voltage < self.buses_volatage_rule['min'] or voltage > self.buses_volatage_rule["max"]:
-                self._state = self.switch[0]
+                self.state = 0
                 while True:
                     try:
                         self.broadcast_message(self._state)
@@ -46,8 +47,30 @@ class AgentCB(ComBase):
         if value == 0 or value == 1:
             print(self.name, "changing to ", self.switch[value])
             self._state = self.switch[value]
+            if self._callback:
+                self._callback(value)
         else:
             print("state can either be '1' or '0'")
+
+class AgentB(ComBase):
+    _voltage = None
+    def __init__(self, name:str, host:str="", port:int=10001, voltage:float=33) -> None:
+        super().__init__(name, host, port)
+        self.voltage = voltage
+        if not self.schedule_attr_broadcast("voltage", 1):
+            raise(Exception("Error Scheduling broadcast"))
+    
+    @property
+    def voltage(self):
+        return self._state
+    
+    @voltage.setter
+    def voltage(self, value):
+        if value >= 0:
+            print(self.name, "changing to ", value)
+            self._state = value
+        else:
+            print("voltage cannot be negative")
 
 
 
