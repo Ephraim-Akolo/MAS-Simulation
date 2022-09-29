ba13682c8df5cac79b5ca66ba4c0877747e8b33b
jake-ephraim <ephraimakolo2017@gmail.com>
2022-09-29 11:27:43 +0100

    inherited methods from combase

--- mas_lib/agent.py
+++ mas_lib/agent.py
@@ -2,4 +2,11 @@ from communications import ComBase
 
 
 class Agent(ComBase):
-    pass
+    def __init__(self, name, host=..., port=10001) -> None:
+        super().__init__(name, host, port)
+    
+    def broadcast(self, name, state):
+        return super().broadcast(name, state)
+    
+    def broadcast_message(self, message: str):
+        return super().broadcast_message(message)
