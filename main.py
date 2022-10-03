de53ce4914a071fb95e1accd2f1be13328dcb40a
jake-ephraim <ephraimakolo2017@gmail.com>
2022-10-03 16:57:48 +0100

    update

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from mas_lib.agent import AgentCB, AgentB
-ag = AgentCB("CB7A")
-print(ag.neighbors, ag.state)
-ab = AgentB("B7")
-AgentCB("CB7B")
-AgentCB("CB6A")
 from time import sleep
-for i in range(33):
-    ab.voltage = ab.voltage - 1
-    sleep(.5)
+from mas_lib.agent import AgentCB, AgentB, AgentDG, AgentSource
+
+source = AgentSource('SOURCE33V')
+dg = [
+    AgentDG("DG1")
+]
+cb = [
+    AgentCB("CB1A"),
+    AgentCB("CB1B"),
+    AgentCB("CB2A"),
+    AgentCB("CB2B"),
+    AgentCB("CB3A"),
+    AgentCB("CB4A")
+    ]
+b =[
+    AgentB("B1"),
+    AgentB("B2"),
+    AgentB("B3"),
+    AgentB("B4"),
+]
+
+sleep(3)
+print("STARTING NETWORK")
+source.reset_network()
+# sleep(5)
+input()
+# source.reset_network()
+print("Breaking!!!")
+b[1].broken = True
+b[1].voltage = 0
+input()
+print("integrating cb4a")
+cb[-1].dggg = True
 input()
 
